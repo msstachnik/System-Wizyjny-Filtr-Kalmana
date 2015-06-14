6a027f8a327ba98ff98b33bb8bebed037c84fd04
Mateusz Stachnik <msstachnik@gmail.com>
2015-06-14 10:55:06 +0200

    V4_3
    Added System_Wizyjny_Online_V4

--- System_Wizyjny_Online_V4.m
+++ System_Wizyjny_Online_V4.m
@@ -1,4 +1,5 @@
-% System_Wizyjny_Online_V3 - zaimplementowany filtr Kalmana
+% System_Wizyjny_Online_V4 - Zaimplementowany filtr Kalmana i optymalizacja
+% okna pomiarowego
 clc
 clear all
 close all
@@ -7,6 +8,11 @@ MaxTime = 20 ; %maksymalny czas pobierania danych
 display_figure = 'ON'; % jeœli 'ON' to wyœwietlany jest pogl¹d aktualnej pozycji
 odleglosc_wierzcholkow=20; % odleglosc w cm miêdzy wierzcho³kami kwadratu ma³ego - u¿ywane w kalibracji
 D_prog = 40; % maksymalna odloeg³osc pomiêdzy dwoma pobranymi punktami - warunek odsiewu szumu pomiarowego
+srednica_pilki = 5; %srednica pilki w cm
+
+% poziom sigma okreœla kompromis pomiêdzy optymalizacj¹ obliczeniow¹  a
+% dok³adnoœci¹ pomiarów. Im wiêkszy poziom sigma tym wiêksze okno pomiarowe
+poziom_sigma = 6;  % poziom sigma nie powininem byæ mniejszy ni¿ 1.5
 
 %% parametry filtru kalmana
 % e_a_sigma -  odchylenie standardowe szumu procesu okreœla jakie mo¿e
@@ -19,8 +25,8 @@ e_pomiar_sigma = 2;
 %% inicjalizacja filtru kalmana
 % filtr kalmana rozbito na 2 filtry osobny dla wspó³¿ednej x i osobny dla
 % wspó³¿ednej y
-Px = zeros(2,2); % macierz kowariancji szumu procesu w filtrze kalmana
-Py = zeros(2,2);
+% Px = zeros(2,2); % macierz kowariancji szumu procesu w filtrze kalmana
+% Py = zeros(2,2);
 R = e_pomiar_sigma^2; %mo¿na przyjac za sta³e mo¿na uzalse¿niæ od dt
 B = [0; 0];         % macierz sta³a w czasie
 C = [1 0];          % macierz sta³a w czasie
@@ -81,6 +87,10 @@ time(1) = 0;
 tic
 %% pobieranie danych
 while(time(i) <MaxTime)
+    %% optymalizacja okan pobierajacego dane
+    
+    
+    
     i=i+1;
     
     
@@ -89,23 +99,79 @@ while(time(i) <MaxTime)
     time(i) = toc;
      
     if i == 2 % pobieranie rozmiarów okna
-        size_film = size(obraz_badany);
+        size_film = size(data);
         wsp_x_max = size_film(2);
         wsp_y_max = size_film(1);
     end
     
+    
+    if k >= 2 % optymalizacja jest w³¹czona dopiero po zaakceptowaniu punktu pocz¹tkowego
+        % i - obecna iteracja, (time(k) / dt) - ostatnia zaakceptowana iteracja pozycji
+        dt_okna = (i - (time(k) / dt)) * dt;         
+        
+        % estymacja po³o¿enia X Y (nie licz¹c wariancji)
+        X_est = X_k(k) + Vx_k(k) * dt_okna;
+        Y_est = Y_k(k) + Vy_k(k) * dt_okna;
+        
+        % konwersja estymacji po³ozenia X Y po wspó³¿êdnych wsp_x wsp_y
+        wsp_x_okna = round(X_est / piks_to_cm);
+        wsp_y_okna = round(wsp_y_max - (Y_est / piks_to_cm));
+        
+        %sprawdzenia czy wspó³¿êdne nie znajduj¹ sie poza obszarem filmu
+        wsp_x_okna = down_up_limit(wsp_x_okna, 1, wsp_x_max);
+        wsp_y_okna = down_up_limit(wsp_y_okna, 1, wsp_y_max);
+
+        % bok okna wynika z statystyki. odchylenia stadardowe b³edu pozycji
+        % z macierzy stany wynosz¹ Px(1,1), prêdkosci Px(2,2), a b³¹d
+        % pomiaru e_pomiar_sigma
+        sigma_x = Px(1,1);
+        sigma_Vx = Px(2,2) * dt_okna;
+        sigma_y = Py(1,1);
+        sigma_Vy = Py(2,2) * dt_okna;
+        % rzeczywista signa u¿ywana do obliczania boku prostok¹ta na
+        % podstawie poziomu sigma
+        real_sigma = down_up_limit(poziom_sigma - 1.5, 0.1, inf);
+        
+        
+
+        % obliczenie boków prostok¹ta - estymacja dok³¹dnosci pomiaru
+        bok_okna_x = srednica_pilki + real_sigma * sqrt(sigma_x^2 + sigma_Vx^2 + e_pomiar_sigma^2);
+        bok_okna_y = srednica_pilki + real_sigma * sqrt(sigma_y^2 + sigma_Vy^2 + e_pomiar_sigma^2);
+        
+        % konwersja na wspó³¿êdne
+        wsp_bok_okna_x = round( bok_okna_x / piks_to_cm);
+        wsp_bok_okna_y = round( bok_okna_y / piks_to_cm);
+        
+        % wyliczenie pozcyji skrajnych prostok¹ta
+        wsp_xmin = down_up_limit(wsp_x_okna - wsp_bok_okna_x, 1, wsp_x_max);
+        wsp_ymin = down_up_limit(wsp_y_okna - wsp_bok_okna_y, 1, wsp_y_max);
+        wsp_xmax = down_up_limit(wsp_x_okna + wsp_bok_okna_x, 1, wsp_x_max);
+        wsp_ymax = down_up_limit(wsp_y_okna + wsp_bok_okna_y, 1, wsp_y_max);
+        
+    else % jeœli nie ma akceptacji pierwszych 2 punktów to skanuj ca³a przestrzeñ
+        wsp_xmin = 1;
+        wsp_ymin = 1;
+        wsp_xmax = wsp_x_max;
+        wsp_ymax = wsp_y_max;
+        
+    
+    end
+    
+    
     % wyciagniecie obiektow koloru czerwonego poprzez odejmowanie od siebie
     % obrazow
+    data = data(wsp_ymin:wsp_ymax,wsp_xmin:wsp_xmax,:);
     obraz_badany = imsubtract(data(:,:,1), rgb2gray(data));
     %funkcja napisana do wykrywania pozycji kulki
     [wsp_x, wsp_y] = image2ball_pos(obraz_badany);
     
     if strcmp(display_figure, 'ON')
         imshow(data)
+        hold on
     end
 
     
-    hold on
+    
     
     %% jêsli wykryto pozycje kólki to zapisaæ
     if not(isnan(wsp_x + wsp_y)) 
@@ -175,7 +241,13 @@ while(time(i) <MaxTime)
                     % pomiar
                     x_state = [X_k(1); Vx_k(1)];
                     y_state = [Y_k(1); Vy_k(1)];
-
+                    
+                    % obliczanie rzeczywistego przyrostu czasu
+                    dt_real = time_ball(k) - time_ball(k-1);
+                    
+                    % oliczenie inicjalizacji macierzy kowariancji b³êdu
+                    Px = diag([e_pomiar_sigma, e_a_sigma * dt_real]); % macierz kowariancji szumu procesu w filtrze kalmana
+                    Py = Px;             
 
                 end
                 %% w³asciwa filtracja kalmana
